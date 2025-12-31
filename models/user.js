42b119520cc384609656f538bf17075f5b9c2ab9
amit das <amit2512das@gmail.com>
2025-12-31 18:46:24 +0530

    done

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema=new Schema({ // but in our schema, there is 3 sections: username, email, password
+    email:{
+        type:String,
+        required:true,  
+    }
+})
+
+User.plugin(passportLocalMongoose);// we dont need to create username, password or have to hash, 
+///this plugin will do all hash , salt, hashed passwords automatically
+
+module.exports = mongoose.model("User", userSchema);
